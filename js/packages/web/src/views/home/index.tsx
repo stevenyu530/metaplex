c892519fd3fd30e4470f6c03791eb5bc20f4cd8f
Libo Shen <liboooshen@gmail.com>
2021-09-16 21:20:55 +0100

    Add intro texts.

--- js/packages/web/src/views/home/index.tsx
+++ js/packages/web/src/views/home/index.tsx
@@ -13,6 +13,16 @@ export const HomeView = () => {
 
   return (
     <Layout style={{ margin: 0, marginTop: 30, alignItems: 'center' }}>
+    <div className="intro">
+      <div>
+        <div className="intro-word"> Mooncake </div>
+        <div> A Chinese bakery product traditionally eaten during the <a href="https://www.google.com/search?q=mid-autumn+festival">Mid-Autumn Festival</a>. Often been gifted to family and friends to give best wishes. </div>
+      </div>
+      <div>
+        <div className="intro-word"> Mooncake NFT </div>
+        <div> A humble virtual (and <a href="https://explorer.solana.com/">on chain</a>) mooncake. <i>Dairy free</i>. <i>Zero calorie</i>. Keep you good mood when added to your wallet. Also a great gift for your (crypto) friends. </div>
+      </div>
+    </div>
       {showAuctions ? <AuctionListView /> : <SetupView />}
     </Layout>
   );
